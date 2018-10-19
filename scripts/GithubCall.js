5f825d263e8e84e98008b569ddbaa58f3da2efb7
Louis7777 <Louis7777@users.noreply.github.com>
2018-10-19 12:45:37 +0300

    Create GithubCall.js

---
+++ scripts/GithubCall.js
@@ -0,0 +1,35 @@
+main = function() {
+
+	githubCall = new XMLHttpRequest;
+
+	githubCall.onreadystatechange=function() {
+		if (githubCall.readyState == 4 && githubCall.status == 200) {
+			document.getElementsByClassName('github-call')[0].innerHTML = eval(githubCall.response);
+		}
+	};
+
+	githubCall.open('GET','https://api.github.com/users/louis7777/repos?callback=myjsonp');
+
+	githubCall.send();
+
+	myjsonp = function (res) {
+		var string = '<ul>';
+		for (i in res.data) {
+			var data = res.data[i];
+			if (data.description == null) {
+				data.description = '';
+			}
+
+			var link = '<a title="'+data.description+'" href="'+data.html_url+'">'+data.name+'</a>';
+			var description = '<span>'+data.description+' - language: '+data.language+'</span>';
+
+			string += '<li>' + link + description + '</li>';
+		}
+		string += '</ul>';
+		return string;
+	};
+};
+
+window.onload = function() {
+	window.setTimeout(main, 2000);
+}
